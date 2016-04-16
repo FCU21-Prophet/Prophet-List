f8020918d4a6a3d6d96803513a266a807e69dab9
a3104 <a3104123@gmail.com>
2016-04-16 20:45:38 +0800

    Set intent for FAB to call newTask

--- app/src/main/java/team/prophet/a3104/prophet_list/MainActivity.java
+++ app/src/main/java/team/prophet/a3104/prophet_list/MainActivity.java
@@ -1,19 +1,19 @@
 package team.prophet.a3104.prophet_list;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,8 +29,13 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                //Intent for call newTask
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, newTask.class);
+                startActivity(intent);
             }
         });
 
