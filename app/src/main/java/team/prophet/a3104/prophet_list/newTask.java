2cb2ff743e0df3300abd0109daf3cb0070c99419
a3104 <a3104123@gmail.com>
2016-04-16 19:59:26 +0800

    Create newTask activity

---
+++ app/src/main/java/team/prophet/a3104/prophet_list/newTask.java
@@ -0,0 +1,13 @@
+package team.prophet.a3104.prophet_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
+}
